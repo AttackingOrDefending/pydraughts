481a63b68760d8905b1d931527c4e72b6108c131
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2021-08-12 11:03:19 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pydraughts",
-    version="0.0.5",
+    version="0.0.6",
     author="AttackingOrDefending",
     description="A draughts library for Python",
     long_description=long_description,
