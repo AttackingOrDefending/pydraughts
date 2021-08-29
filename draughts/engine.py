2a1581b9412cc682043f43a6d599fb758b2a8507
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2021-08-29 17:50:50 +0300

    Fix engine bug

---
+++ draughts/engine.py
@@ -0,0 +1,12 @@
+from draughts.engines.dxp import DXPEngine
+from draughts.engines.hub import HubEngine
+
+
+class Limit:
+    def __init__(self, time=None, inc=None, depth=None, nodes=None, movetime=None):
+        assert time is not None or depth is not None or nodes is not None or movetime is not None
+        self.time = time
+        self.inc = inc
+        self.depth = depth
+        self.nodes = nodes
+        self.movetime = movetime
