6e91771ce67be649e93ebe5709f08c5a9b84d87b
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2021-09-25 17:52:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pydraughts",
-    version="0.2.3",
+    version="0.2.4",
     author="AttackingOrDefending",
     description="A draughts library for Python with move generation and engine communication",
     long_description=long_description,
