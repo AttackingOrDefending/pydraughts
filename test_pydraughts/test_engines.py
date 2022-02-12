01574fdb7030677afc06609ac90bdd26a71389c0
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2022-02-12 15:14:55 +0200

    Add tests and fix italian bug (#4)
    Added tests and fixed a bug in italian draughts

---
+++ test_pydraughts/test_engines.py
@@ -0,0 +1,146 @@
+import draughts
+from draughts.engine import HubEngine, DXPEngine, CheckerBoardEngine, Limit
+
+import pytest
+import requests
+import zipfile
+import shutil
+import os
+import stat
+import sys
+import logging
+platform = sys.platform
+file_extension = '.exe' if platform == 'win32' else ''
+
+logger = logging.getLogger(__name__)
+
+
+def download_scan():
+    windows_linux_mac = ''
+    if platform == 'linux':
+        windows_linux_mac = '_linux'
+    elif platform == 'darwin':
+        windows_linux_mac = '_mac'
+    response = requests.get('https://hjetten.home.xs4all.nl/scan/scan_31.zip', allow_redirects=True)
+    with open('./TEMP/scan_zip.zip', 'wb') as file:
+        file.write(response.content)
+    with zipfile.ZipFile('./TEMP/scan_zip.zip', 'r') as zip_ref:
+        zip_ref.extractall('./TEMP/')
+    shutil.copyfile(f'./TEMP/scan_31/scan{windows_linux_mac}{file_extension}', f'scan{file_extension}')
+    shutil.copyfile('./TEMP/scan_31/scan.ini', 'scan.ini')
+    if os.path.exists('data'):
+        shutil.rmtree('data')
+    shutil.copytree('./TEMP/scan_31/data', 'data')
+    if windows_linux_mac != "":
+        st = os.stat(f'scan{file_extension}')
+        os.chmod(f'scan{file_extension}', st.st_mode | stat.S_IEXEC)
+
+
+def download_kr():
+    response = requests.get('http://edgilbert.org/InternationalDraughts/downloads/kr_hub_163.zip', allow_redirects=True)
+    with open('./TEMP/kr_zip.zip', 'wb') as file:
+        file.write(response.content)
+    with zipfile.ZipFile('./TEMP/kr_zip.zip', 'r') as zip_ref:
+        zip_ref.extractall('./TEMP/')
+    shutil.copyfile('./TEMP/kr_hub.exe', 'kr_hub.exe')
+    shutil.copyfile('./TEMP/kr_hub.ini', 'kr_hub.ini')
+    shutil.copyfile('./TEMP/KingsRow.odb', 'KingsRow.odb')
+    shutil.copyfile('./TEMP/weights.bin', 'weights.bin')
+
+
+def download_cake():
+    headers = {'User-Agent': 'User Agent', 'From': 'mail@mail.com'}
+    response = requests.get('http://www.fierz.ch/cake_189f.zip', headers=headers)
+    with open('./TEMP/cake_zip.zip', 'wb') as file:
+        file.write(response.content)
+    with zipfile.ZipFile('./TEMP/cake_zip.zip', 'r') as zip_ref:
+        zip_ref.extractall('./TEMP/')
+    shutil.copyfile('./TEMP/cake_189f.dll', 'cake_189f.dll')
+    shutil.copyfile('./TEMP/egdb64.dll', 'egdb64.dll')
+    shutil.copyfile('./TEMP/book.bin', 'book.bin')
+
+
+if os.path.exists('TEMP'):
+    shutil.rmtree('TEMP')
+os.mkdir('TEMP')
+download_scan()
+download_kr()
+download_cake()
+
+
+@pytest.mark.timeout(300, method="thread")
+def test_hub_dxp_engines():
+    if platform != 'win32':
+        assert True
+        return
+    hub = HubEngine('kr_hub.exe')
+    dxp = DXPEngine(['scan.exe', 'dxp'], {'engine-opened': False}, initial_time=30)
+    limit = Limit(10)
+    game = draughts.Game()
+    logger.info('Starting game 1')
+    while not game.is_over() and len(game.move_stack) < 100:
+        logger.info(f'move1: {len(game.move_stack)}')
+        if len(game.move_stack) % 2 == 0:
+            best_move = dxp.play(game)
+        else:
+            best_move = hub.play(game, limit, False)
+        if best_move.move:
+            for move in best_move.move.board_move:
+                game.move(move)
+        else:
+            break
+    logger.info('Finished playing 1')
+    dxp.quit()
+    logger.info('Quitted dxp 1')
+    dxp.kill_process()
+    logger.info('Killed dxp 1')
+    hub.quit()
+    logger.info('Quited hub 1')
+    hub.kill_process()
+    logger.info('Killed hub 1')
+    hub = HubEngine('kr_hub.exe')
+    dxp = DXPEngine(['scan.exe', 'dxp'], {'engine-opened': False}, initial_time=30)
+    limit = Limit(10)
+    game = draughts.Game()
+    logger.info('Starting game 2')
+    while not game.is_over() and len(game.move_stack) < 100:
+        logger.info(f'move2: {len(game.move_stack)}')
+        if len(game.move_stack) % 2 == 1:
+            best_move = dxp.play(game)
+        else:
+            best_move = hub.play(game, limit, False)
+        if best_move.move:
+            for move in best_move.move.board_move:
+                game.move(move)
+        else:
+            break
+    logger.info('Finished playing 2')
+    dxp.quit()
+    logger.info('Quitted dxp 2')
+    dxp.kill_process()
+    logger.info('Killed dxp 2')
+    hub.quit()
+    logger.info('Quited hub 2')
+    hub.kill_process()
+    logger.info('Killed hub 2')
+
+
+@pytest.mark.timeout(150, method="thread")
+def test_checkerboard_engines():
+    if platform != 'win32':
+        assert True
+        return
+    checkerboard = CheckerBoardEngine('cake_189f.dll')
+    limit = Limit(10, 2)
+    game = draughts.Game(variant='english')
+    logger.info('Starting game 1')
+    while not game.is_over() and len(game.move_stack) < 100:
+        logger.info(f'move1: {len(game.move_stack)}')
+        best_move = checkerboard.play(game, limit)
+        if best_move.move:
+            for move in best_move.move.board_move:
+                game.move(move)
+        else:
+            break
+    logger.info('Finished playing 1')
+    checkerboard.kill_process()
---
+++ test_pydraughts/test_game.py
@@ -0,0 +1,13 @@
+from draughts import Game, WHITE
+
+
+def test_game():
+    assert Game('breakthrough', 'B:WK4,31,35,36,38,40,43,44,45,46,47,48,49,50:B1,2,3,6,7,8,9,11,13,16').is_over()
+    assert Game('breakthrough', 'B:WK4,31,35,36,38,40,43,44,45,46,47,48,49,50:B1,2,3,6,7,8,9,11,13,16').get_winner() == WHITE
+
+    game = Game(fen='B:W27,28,32,34,35,37,39,40,42,43,44,45,46,47,48,49,50:B1,2,3,4,5,6,7,8,9,10,12,13,15,16,19,23,24')
+    moves, captures = game.legal_moves()
+    correct_moves = [[[15, 20]], [[10, 14]], [[9, 14]], [[24, 30]], [[24, 29]], [[23, 29]], [[13, 18]], [[12, 18]], [[12, 17]], [[7, 11]], [[6, 11]], [[16, 21]]]
+    correct_moves.sort()
+    correct_captures = [[None]] * 12
+    assert moves == correct_moves and captures == correct_captures
