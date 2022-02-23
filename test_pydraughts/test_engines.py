2f00d59506758bd8459c7d57159f0b1547266211
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2022-02-23 20:53:07 +0200

    Remove quote

--- test_pydraughts/test_engines.py
+++ test_pydraughts/test_engines.py
@@ -95,7 +95,7 @@ def test_hub_engines():
     if platform not in ['win32', 'linux', 'darwin']:
         assert True
         return
-    hub = HubEngine(['scan.exe', 'hub']')
+    hub = HubEngine(['scan.exe', 'hub'])
     limit = Limit(10)
     game = draughts.Game()
     logger.info('Starting game 1')
