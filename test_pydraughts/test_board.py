69c43f94993ca59a01aa822b4aaf767e9e8f32eb
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2022-04-05 13:41:23 +0200

    Add files via upload

---
+++ test_pydraughts/test_board.py
@@ -0,0 +1,11 @@
+from draughts import Game
+from draughts.core.board import Board
+
+
+def test_board():
+    game = Game()
+    game.board = Board()
+    assert game.get_li_fen() == 'W:W31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50:B1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20'
+    assert game.board.count_movable_player_pieces() == 5
+    game.board, _ = game.board.create_new_board_from_move([35, 30], 1, [])
+    assert game.get_li_fen() == 'B:W30,31,32,33,34,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50:B1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20'
--- test_pydraughts/test_convert.py
+++ test_pydraughts/test_convert.py
@@ -4,8 +4,10 @@ from draughts.convert import fen_from_variant, fen_to_variant, move_from_variant
 def test_convert():
     assert fen_from_variant('B:W19,21,22,23,25,26,27,29-31,32:B1,2,3-5,6,7,9,10,11,12', variant='english') == 'W:W21,22,23,24,26,27,28,29,30,31,32:B1,10,11,12,14,2,3,4,6,7,8'
     assert fen_from_variant('W:Wa3,Kc3,Ke3,g3,b2,d2,f2,h2,a1,c1,e1,g1:Bb8,d8,f8,h8,a7,c7,e7,g7,b6,Kd6,Kf6,h6', variant='russian') == 'W:W21,24,25,26,27,28,29,30,31,32,K22,K23:B1,12,2,3,4,5,6,7,8,9,K10,K11'
+    assert fen_from_variant('W:WK12-14:BK28-31', variant='standard') == 'W:WK12,K13,K14:BK28,K29,K30,K31'
     assert fen_to_variant('W:W21,22,23,24,26,27-29,30,31,32:B1,10,11,12,14,2-4,6,7,8', variant='english') == 'B:W19,21,22,23,25,26,27,29,30,31,32:B1,10,11,12,2,3,4,5,6,7,9'
     assert fen_to_variant('B:W17,22,23,24,25,26,27-29,30,31,32:B1,2,3-5,6,7,8,9,10,11,12', variant='russian') == 'B:Wa1,c3,e3,g3,b4,c1,e1,g1,b2,d2,f2,h2:Bb6,d6,f6,h6,a7,c7,e7,g7,b8,d8,f8,h8'
+    assert fen_to_variant('W:WK12-14:BK28-31', variant='standard') == 'W:WK12,K13,K14:BK28,K29,K30,K31'
 
     assert move_from_variant('g3-h4', variant='russian') == '24-20'
     assert move_to_variant('24-20', variant='russian') == 'g3-h4'
@@ -14,3 +16,4 @@ def test_convert():
     assert _number_to_algebraic('1-9', variant='turkish') == 'a1-a2'
     assert _number_to_algebraic('a1-a2', variant='turkish') == 'a1-a2'
     assert _numeric_to_algebraic_square('32', width=4) == 'h8'
+    assert _numeric_to_algebraic_square('h8', width=4) == 'h8'
