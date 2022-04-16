473719a9d7d421059d88d830bdeaa1d7ef9e44be
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2022-04-16 15:50:09 +0300

    Update test_engines.py

--- test_pydraughts/test_engines.py
+++ test_pydraughts/test_engines.py
@@ -283,11 +283,6 @@ def test_engines():
     hub.quit()
     hub.kill_process()
 
-    # Unexpected response to hub
-    hub_command_thread = threading.Thread(target=HubEngine, args=[f'scan{file_extension}'])
-    hub_command_thread.start()
-    hub_command_thread.join(2)
-
     # Unexpected engine response to init
     with open('scan.ini') as file:
         options = file.read()
