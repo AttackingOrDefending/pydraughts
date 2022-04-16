0c66066efaa981d4ecd763eb58ef7d01d3e5d2a3
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2022-04-16 20:49:38 +0300

    Update test_dxp_communication.py

--- test_pydraughts/test_dxp_communication.py
+++ test_pydraughts/test_dxp_communication.py
@@ -1,4 +1,5 @@
 from draughts.engines.dxp_communication import dxp_run
+from draughts.engines.dxp_communication.dxp_classes import DamExchange
 from importlib import reload
 from draughts import Game
 import logging
@@ -55,4 +56,79 @@ def test_console_handler():
     tConsoleHandler.run_command('random-command')
 
 
-test_console_handler()
+from draughts.engines.dxp_communication import dxp_run
+from draughts.engines.dxp_communication.dxp_classes import DamExchange
+from importlib import reload
+from draughts import Game
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger("pydraughts")
+logger.setLevel(logging.DEBUG)
+
+
+def test_console_handler():
+    global dxp_run
+
+    # terminate program (doesn't do anything)
+    dxp_run = reload(dxp_run)
+    tConsoleHandler = dxp_run.ConsoleHandler()
+    tConsoleHandler.run_command('setup')
+    tConsoleHandler.run_command('q')
+
+    # setup (number of commands = 2)
+    dxp_run = reload(dxp_run)
+    tConsoleHandler = dxp_run.ConsoleHandler()
+    tConsoleHandler.run_command(f'setup {Game().get_dxp_fen()}')
+
+    # setup (number of commands > 3)
+    dxp_run = reload(dxp_run)
+    tConsoleHandler = dxp_run.ConsoleHandler()
+    tConsoleHandler.run_command(f'setup {Game().get_dxp_fen()} {Game().variant} extra')
+
+    dxp_run = reload(dxp_run)
+    tConsoleHandler = dxp_run.ConsoleHandler()
+    # conn (number of commands = 2) & Error trying to connect: connection exception: failed to connect
+    tConsoleHandler.run_command('conn 127.0.0.1')
+    # conn (number of commands = 1) & Error trying to connect: connection exception: failed to connect
+    tConsoleHandler.run_command('conn')
+    # chat (number of commands = 1)
+    tConsoleHandler.run_command('chat')
+    # chat (number of commands > 1) & Error sending chat message: send exception: no connection
+    tConsoleHandler.run_command('chat message')
+    # gamereq (number of commands = 2) & Error sending game request: send exception: no connection
+    tConsoleHandler.run_command('gamereq W')
+
+    dxp_run = reload(dxp_run)
+    tConsoleHandler = dxp_run.ConsoleHandler()
+    # gamereq (number of commands = 3) & Error sending game request: send exception: no connection
+    tConsoleHandler.run_command('gamereq B 100')
+    # Game already finished; gameend not allowed
+    tConsoleHandler.run_command('gameend')
+
+    dxp_run = reload(dxp_run)
+    tConsoleHandler = dxp_run.ConsoleHandler()
+    # Game not started; backreq not allowed
+    tConsoleHandler.run_command('backreq')
+    # Command unknown
+    tConsoleHandler.run_command('random-command')
+
+
+def test_dam_exchange():
+    dam_exchange = DamExchange()
+
+    assert dam_exchange.parse('CMESSAGE') == {'type': 'C', 'text': 'MESSAGE'}  # CHAT
+    assert dam_exchange.parse('B001W') == {'type': 'B', 'moveId': '001', 'mColor': 'W'}  # BACKREQ
+    assert dam_exchange.parse('K1') == {'type': 'K', 'accCode': '1'}  # BACKACC
+    assert dam_exchange.parse('P0') == {'type': '?'}  # Unknown
+    assert dam_exchange.parse('E11') == {'type': 'E', 'reason': '1', 'stop': '1'}  # GAMEEND
+    assert dam_exchange.parse('ANAME                            1') == {'type': 'A', 'engineName': 'NAME', 'accCode': '1'}  # GAMEACC
+    assert dam_exchange.parse('M000135240130') == {'type': 'M', 'time': '0001', 'from': '35', 'to': '24', 'nCaptured': '01', 'captures': ['30']}  # MOVE
+    assert dam_exchange.parse(f'R01NAME                            Z010100BW{Game().get_dxp_fen()}') == {'type': 'R', 'name': 'NAME', 'fColor': 'Z', 'gameTime': '010', 'numMoves': '100', 'posInd': 'B', 'mColor': 'W', 'pos': 'zzzzzzzzzzzzzzzzzzzzeeeeeeeeeewwwwwwwwwwwwwwwwwwww'}  # GAMEREQ
+
+    assert dam_exchange.parse(dam_exchange.msg_gamereq(0, 100, 100, Game(), 0)) == {'type': 'R', 'name': 'DXP Client', 'fColor': 'Z', 'gameTime': '100', 'numMoves': '100', 'posInd': 'B', 'mColor': 'W', 'pos': 'zzzzzzzzzzzzzzzzzzzzeeeeeeeeeewwwwwwwwwwwwwwwwwwww'}  # GAMEREQ
+    assert dam_exchange.msg_gamereq(0, 100, 100) == 'R01DXP Client                      Z100100A'  # GAMEREQ
+    assert dam_exchange.parse(dam_exchange.msg_backreq(1, 1)) == {'type': 'B', 'moveId': '001', 'mColor': 'Z'}  # BACKREQ
+    assert dam_exchange.msg_backreq(1, 0) == 'B001W'  # BACKREQ
+    assert dam_exchange.parse(dam_exchange.msg_backacc('1')) == {'type': 'K', 'accCode': '1'}  # BACKACC
+    assert dam_exchange.msg_backacc('0') == 'K0'  # BACKACC
