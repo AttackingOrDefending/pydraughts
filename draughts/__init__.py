7f3921097416934a537a00ea8de9ebd4bdd576ea
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2022-04-25 19:54:59 +0300

    Add author, copyright and version in __init__.py

--- draughts/__init__.py
+++ draughts/__init__.py
@@ -1,2 +1,6 @@
 from draughts.core.game import Game, WHITE, BLACK
 from draughts.core.move import Move
+
+__author__ = "AttackingOrDefending"
+__copyright__ = "2021-2022, " + __author__
+__version__ = "0.5.5"
