23c72999f7f08b7239385b3adffbec9b8d89d760
Ioannis Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2023-06-07 15:50:51 +0300

    0.6.2

--- draughts/__init__.py
+++ draughts/__init__.py
@@ -2,6 +2,6 @@ from draughts.core.game import Game
 from draughts.core.move import StandardMove
 from draughts.core.variant import Board, Move, WHITE, BLACK
 
-__author__ = "AttackingOrDefending"
+__author__ = "Ioannis Pantidis"
 __copyright__ = "2021-2022, " + __author__
-__version__ = "0.6.1"
+__version__ = "0.6.2"
