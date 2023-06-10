ad29ea35358dfe86305bd850199a57e2066f341e
Ioannis Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2023-06-10 19:32:33 +0300

    Don't update version

--- draughts/__init__.py
+++ draughts/__init__.py
@@ -2,4 +2,4 @@ from draughts.core.variant import Board, Move, WHITE, BLACK
 
 __author__ = "Ioannis Pantidis"
 __copyright__ = "2021-2023, " + __author__
-__version__ = "0.7.0"
+__version__ = "0.6.3"
