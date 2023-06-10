0030fc008fa3909acc94c9458896a53091c26c5e
Ioannis Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2023-06-10 20:46:55 +0300

    Add tournament manager (#37)
    * P1
    
    * Don't update version
    
    * Tests
    
    * Tests 2

---
+++ test_pydraughts/test_tournament.py
@@ -0,0 +1,20 @@
+from draughts.tournament import RoundRobin
+import pytest
+import sys
+import logging
+platform = sys.platform
+file_extension = '.exe' if platform == 'win32' else ''
+
+logging.basicConfig()
+logger = logging.getLogger("pydraughts")
+logger.setLevel(logging.DEBUG)
+
+@pytest.mark.timeout(300, method="thread")
+def test_tournament():
+    if platform != 'win32':
+        assert True
+        return
+    players = [(["scan.exe", "dxp"], "dxp", {'engine-opened': False}), ("kr_hub.exe", "hub", {})]
+    tournament = RoundRobin("tournament.pdn", players, 20, .2)
+    scores = tournament.play()
+    tournament.print_standings()
